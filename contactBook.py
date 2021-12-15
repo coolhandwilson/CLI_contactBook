c979342bb01e15aea3d16139ad37f3d1176fbdd6
Wilson Scott <wscott53@my.bcit.ca>
2021-12-14 22:37:11 -0800

    Add code, helper function for creation of book

--- contactBook.py
+++ contactBook.py
@@ -13,6 +13,33 @@ def display_contact_types(sub_books: list):
         print("Book {:<2}{:*>25}".format(number + 1, sub_books[number]))
 
 
+def new_sublist(new_file: bool = False):
+    categories = ['Friends', 'Business', 'School', 'Family']
+
+    while True:
+        print("Please choose a new sub-category: \n")
+        display_contact_types(categories)
+        choice = input("Please enter the corresponding number of the category you wish to create.\n")
+
+        if choice not in [number for number in range(1, len(categories) + 1)]:
+            print("Invalid input. Please enter numeric values only.\n")
+
+        else:
+            choice = int(choice)
+            print(f"Wonderful. You have selected {categories[choice - 1]}.")
+            return categories[choice - 1]
+
+
+def prompt_new_book(*file_name: tuple):
+    """
+
+    :return:
+    """
+    print("Hello! you don't seem to have any entries in your address book. Let's start with making a new category.")
+    if len(file_name) == 0:
+        user_choice = new_sublist()
+
+
 
 
 
